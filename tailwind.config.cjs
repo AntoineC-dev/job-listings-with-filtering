1f5ec86de45a25ebd3dfddbc446bdc9b9a7238de
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-08 17:06:30 -0400

    Configure challenge default styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{svelte,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: {
+        DEFAULT: "hsl(180, 29%, 50%)",
+      },
+      neutral: {
+        100: "hsl(180, 52%, 96%)",
+        200: "hsl(180, 31%, 95%)",
+        300: "hsl(180, 8%, 52%)",
+        400: "hsl(180, 14%, 20%)",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["League\\ Spartan", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
