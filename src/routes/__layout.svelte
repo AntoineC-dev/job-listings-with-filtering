426d4442da3ab2ed74a4685068c3f0f17802f383
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-09 11:04:22 -0400

    Display /= bg image depending on breakpoint

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
   import "../app.css";
+
+  let innerWidth = 0;
+  $: src = innerWidth > 640 ? "./images/bg-header-desktop.svg" : "./images/bg-header-mobile.svg";
 </script>
+
+<svelte:window bind:innerWidth />
+
+<header class="bg-primary relative -z-50">
+  <img {src} alt="" class="w-full h-[10.4rem] object-cover object-center" />
+</header>
 
 <slot />
