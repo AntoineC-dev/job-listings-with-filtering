e04d7809d8657490a98d44f865e11b4b2acbeb48
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-09 11:02:49 -0400

    Create custom breakpoint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/app.html", "./src/**/*.{svelte,ts}"],
   theme: {
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -10,6 +14,7 @@ module.exports = {
         DEFAULT: "hsl(180, 29%, 50%)",
       },
       neutral: {
+        DEFAULT: "hsl(0, 0%, 100%)",
         100: "hsl(180, 52%, 96%)",
         200: "hsl(180, 31%, 95%)",
         300: "hsl(180, 8%, 52%)",
